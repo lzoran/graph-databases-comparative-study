29f296bf9c8d2075157db12181137e3ec4ce7551
Zoran Luledzija <zoran.luledzija@outlook.com>
2017-02-25 23:18:55 +0100

    neo4j base implementation

---
+++ src/test/java/com/graphdatabases/neo4j/PerformanceAnalysisTest.java
@@ -0,0 +1,13 @@
+package com.graphdatabases.neo4j;
+
+import junit.framework.TestCase;
+
+public class PerformanceAnalysisTest extends TestCase{
+
+    // TODO: add unit tests
+
+    public void testDummy()
+    {
+        assertTrue( true );
+    }
+}
